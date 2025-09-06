1c612286363ee56a1eba1d71ac11c0b00281b15d
karthic-rag <karthicragunath7@gmail.com>
2025-09-06 14:58:36 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,14 +10,16 @@ import NavBar from "./components/NavBar";
 const App = () => {
   const theme = useContext(ThemeContext);
   useEffect(() => {
+    const metaThemeColor = document.querySelector("meta[name=theme-color]");
     if (theme.isDark) {
       document.documentElement.classList.add("dark");
-      document.documentElement.classList.remove("light");
+      if (metaThemeColor) metaThemeColor.setAttribute("content", "#040918"); // dark top bar
     } else {
       document.documentElement.classList.remove("dark");
-      document.documentElement.classList.add("light");
+      if (metaThemeColor) metaThemeColor.setAttribute("content", "#EBF2FC"); // light top bar
     }
   }, [theme.isDark]);
+
   return (
     <div className="min-h-screen font-Noto p-4">
       <Header />
